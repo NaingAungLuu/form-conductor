e5f430d94850529d50564e2cac35224339bb4baf
Naing Aung Luu <naingaunglu01@gmail.com>
2022-10-22 23:43:13 +0630

    update: include failedRules set in form result

--- core/src/main/java/me/naingaungluu/formconductor/FormResult.kt
+++ core/src/main/java/me/naingaungluu/formconductor/FormResult.kt
@@ -1,9 +1,16 @@
 package me.naingaungluu.formconductor
 
+import me.naingaungluu.formconductor.validation.ValidationRule
+
 sealed class FormResult<out T : Any> {
+
     data class Success<T : Any> (
         val data: T
     ) : FormResult<T>()
-    object Error : FormResult<Nothing>()
+
+    data class Error(
+        val failedRules: Set<ValidationRule<*, *>>
+    ) : FormResult<Nothing>()
+
     object NoInput : FormResult<Nothing>()
 }

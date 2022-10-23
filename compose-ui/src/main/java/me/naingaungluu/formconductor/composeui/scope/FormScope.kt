b70920f29a951963cf57d775485dc6df383ea91d
Naing Aung Luu <naingaunglu01@gmail.com>
2022-10-23 17:16:31 +0630

    document: compose-ui module

--- compose-ui/src/main/java/me/naingaungluu/formconductor/composeui/scope/FormScope.kt
+++ compose-ui/src/main/java/me/naingaungluu/formconductor/composeui/scope/FormScope.kt
@@ -5,7 +5,20 @@ import androidx.compose.runtime.State
 import me.naingaungluu.formconductor.FormResult
 import kotlin.reflect.KProperty1
 
+/**
+ * A scope interface for Form objects
+ *
+ * @param T
+ */
 interface FormScope<T : Any> {
+
+    /**
+     * Registers the field and returns a singleton FormField instance
+     *
+     * @param V type of field value
+     * @param fieldClass kotlin property reference of the field
+     * @return
+     */
     fun <V : Any> registerField(fieldClass: KProperty1<T, V>): me.naingaungluu.formconductor.FormField<V>
 
     val formState: State<FormResult<T>>

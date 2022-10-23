3f76be5dc738b845bb250d0597b79d59c80df7ee
Naing Aung Luu <naingaunglu01@gmail.com>
2022-10-23 17:10:06 +0630

    document: validation package

--- core/src/main/java/me/naingaungluu/formconductor/validation/ValidationRule.kt
+++ core/src/main/java/me/naingaungluu/formconductor/validation/ValidationRule.kt
@@ -2,6 +2,14 @@ package me.naingaungluu.formconductor.validation
 
 import me.naingaungluu.formconductor.FieldResult
 
+/**
+ * Validation Rule interface
+ *
+ * You can implement this interface to create custom validation rules
+ *
+ * @param T Type of value
+ * @param A Type of associated annotation class
+ */
 interface ValidationRule<T : Any?, A : Annotation> {
     fun validate(value: T, options: A): FieldResult
 }
